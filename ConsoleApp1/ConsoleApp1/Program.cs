1477c21bd4f7a3e68a4dc558b62a380167eb452b
Jess <jess.berry@checkatrade.com>
2021-01-19 12:56:04 +0000

    Importing files to area
    First import of the app to move docs from one area to another.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = @"C:\Users\JessB\Documents\Test App\TestJPEG.jpg";
+            string destinationFile = @"C:\Users\JessB\Documents\Test App Arrival Point\TestJPEG.jpg";
+
+            System.IO.File.Move(sourceFile, destinationFile);
+        }
+    }
+}
